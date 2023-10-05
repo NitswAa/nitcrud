02f02cd72b7685285c69646d87f682ba111c12ae
Austin Witte <nitswaa@gmail.com>
2023-10-04 19:54:56 -0500

    Complete rework (based on original functionality) using the React library + JSX.

--- nitcrud.js
+++ nitcrud.js
@@ -1,72 +1,94 @@
-/*
-A user must be able to create new checklist items.
-    The UI must have a method for the user to add a new item.
-    The backend must accept a request from the UI to save the new item.
-A user must be able to read existing checklist items.
-    Existing checklist items must be presented to the user when the page is loaded.
-    This includes any new items that had been added beyond the initial state.
-    The backend must accept a request from the UI to retrieve all checklist items and their state.
-A user must be able to update existing checklist items.
-    Checklist items must be able to be marked complete in the UI.
-    The text of a checklist item must be able to be modified.
-    ‘a’ and ‘b’ above can be either a single request or two separate requests.
-A user must be able to delete existing checklist items.
-    The UI must have a method for the user to delete an existing checklist item.
-    The backend must accept a request from the UI to delete a checklist item.
-*/
-// requires
-const FileSystem = require("fs");
+const app = document.getElementById("app")
 
-function displayMessage() {
-    document.getElementById("hiddenMessage").style.display = 'block';
+function Header({ title }) {
+    return (
+        <h1>{title ? title : 'Default Title'}</h1>
+    )
 }
 
-// Constructor for checklist items
-function Item(content) {
-    this.content = content;
-    this.isComplete = false;
-    // Probably a useless function
-    this.checkOff = function() {
-        this.isComplete = true;
+// Do I want this function global or in some component? Feel like
+// it can't be in Item...
+function handleDelete() {
+    
+}
+
+// Similar...
+function handleEdit() {
+
+}
+
+function Item({ content, isChecked=false }) {
+    return (
+        <tr> 
+        {/* Can I use keys to reference rows? 
+            Or should I just convert to stylized list... */}
+            <td>{content}</td>
+            {/* I want this (below) to eventually just be a checkbox */}
+            <td>{isChecked ? "True" : "False"}</td>
+            {/* Options: Re-render item content as input form (and replace edit
+                button with submit?) 
+                Or prompt user input and replace */}
+            <td><button onClick={handleDelete}><i class="fa-solid fa-pencil"></i> Edit</button></td>
+            <td><button onClick={handleEdit}><i class="fa fa-trash"></i> Delete</button></td>
+        </tr>
+    )
+}
+
+function Form() {
+    const [items, setItems] = React.useState([])
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    
+        // Read the form data
+        const form = e.target
+        const formData = new FormData(form)
+        
+        const formJson = Object.fromEntries(formData.entries())
+        console.log(formJson)
+
+        // Update item list
+        setItems(
+            [
+                ...items,
+                { content: formJson.itemContent, isChecked: false }
+            ]
+        )
+        console.log(items)
+
+        // Clear text input (there's gotta be a better way?!)
+        document.getElementById("itemInput").value = ''
     }
-    this.checkOn = function() { // Split into two functions because boolean? or set.
-        this.isComplete = false;
-    }
-    this.setContent = function(newContent) {
-        this.content = newContent;
-    }
+
+    return (
+        <div>
+            <form method="post" onSubmit={handleSubmit}>
+                <label>Item</label>
+                <input id="itemInput" name="itemContent" type="text" />
+                <button id="newItemBtn" type="submit">Submit</button>
+            </form>
+            <table>
+            <tr>
+                <th>Item </th>
+                <th>Completed?</th>
+            </tr>
+            {items.map((item) => (
+                <Item content={item.content} isChecked={item.isChecked}/>
+            ))}
+            </table>
+        </div>
+    )
 }
 
-// TODO add error handling
-function addItem(content, loc) {
-    // Basic err catch
-    if (loc > checklist.length)
-        loc = checklist.length;
-    checklist[loc] = new Item(content);
+function HomePage() {
+    
+
+    return (
+        <div>
+            <Header title="A rough To-Do List" />
+            <Form />           
+        </div>
+    )
 }
 
-// Save to JSON file on system... can probably somewhat optimize this in the sense of
-// not having it be run every time something updates? but for now it works
-function save() {
-    FileSystem.writeFile('checklist.json', JSON.stringify(checklist), (error) => {
-        if (error) throw error;
-    });
-}
-
-// Initialize array of checklist items by loading file
-// TODO How to make this independent for each user?
-// Also note doesn't come in as Item object... but is that really even necessary?
-// Obviously could just convert them all, potentially a better way but idk it
-let checklist = require('./checklist.json');
-for (let i = 0; i < checklist.length; i++) {
-    checklist[i] = new Item(checklist[i].content);
-}
-
-// TODO Click functionality?
-
-addItem("Check this off!", 0);
-addItem("Don't check this off yet!", 1);
-checklist[0].checkOff();
-console.log(checklist);
-
-save();
+ReactDOM.render(<HomePage />, app)
